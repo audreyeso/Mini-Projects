ea1b9858d1b83af1a5a34dce9fad88817e9bdea2
Audrey So <audreyeso@Audreys-MacBook-Air.local>
2016-09-18 16:23:00 -0700

    shared preferences practice

--- MyApplication/app/src/main/java/com/test/mini_projects/MainActivity.java
+++ MyApplication/app/src/main/java/com/test/mini_projects/MainActivity.java
@@ -3,11 +3,22 @@ package com.test.mini_projects;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+Use shared preferences to figure out how to load data into your database the first time
+ you app is created. Use shared preferences to make it so the last temperature type
+ that was displayed is loaded when the app is resumed.
+ So if I had last pressed the Celsius button to be displaying the temperature in Celsius,
+ the next time I load the app it should load as Celsius.
+
+ Use Shared Preferences to only insert data into the database 1 time
+ Use Shared Preferences to save the last chosen temperature type.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
